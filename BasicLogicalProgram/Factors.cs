7fda42dab187ca4533f26bbff3429fe819546aaf
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-07 10:16:58 +0530

    Modified to Prime Factor program

---
+++ BasicLogicalProgram/Factors.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogicalProgram
+{
+    public class Factors
+    {
+        public void FactorsNumber()
+        {
+            Console.WriteLine("Enter the input value");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            for (int i = 2; i < input; i++)
+            {
+                if (input % i == 0)
+                {
+                    count = 1;
+                    Console.WriteLine("The {0} is factor of {1}", i, input);
+                }
+                for (int j = 2; j <= i - 1; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count = 0;
+                    }
+                }
+                if (count == 1)
+                {
+                    Console.WriteLine("The {0} is prime factor", i);
+                }
+            }
+        }
+    }
+}
--- BasicLogicalProgram/Program.cs
+++ BasicLogicalProgram/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Bassic Logical Program");
-            Console.WriteLine("1:Flip coin\n2:Leap Year\n3:Power of two\n4:Harmonics\n5:Find Quotient\n6:Swapping Numbers\n7:Even Odd Numbers\n8:Consonant Vowel\n9:Largest Number");
+            Console.WriteLine("1:Flip coin\n2:Leap Year\n3:Power of two\n4:Harmonics\n5:Find Quotient\n6:Swapping Numbers\n7:Even Odd Numbers\n8:Consonant Vowel\n9:Largest Number\n10:Factors of Number");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -44,6 +44,10 @@
                 case 9:
                     LargestNumber largestNumber = new LargestNumber();
                     largestNumber.LargestOfThreeNumber();
+                    break;
+                case 10:
+                    Factors factors = new Factors();
+                    factors.FactorsNumber();
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
