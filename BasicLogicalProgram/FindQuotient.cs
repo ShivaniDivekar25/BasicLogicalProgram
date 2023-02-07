39295c77320883951effff674cb313f0de38a20d
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-07 09:44:19 +0530

    Modified to Find Quotient program

---
+++ BasicLogicalProgram/FindQuotient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogicalProgram
+{
+    public class FindQuotient
+    {
+        public void FindQuotients()
+        {
+            Console.WriteLine("Enter Divident value");
+            int divident = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter divisor value");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            int quotient = Math.DivRem(divident, divisor, out int reminder);
+            Console.WriteLine("The quotient is:" + quotient);
+            Console.WriteLine("The remainder is:" + reminder);
+        }
+    }
+}
--- BasicLogicalProgram/Program.cs
+++ BasicLogicalProgram/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Bassic Logical Program");
-            Console.WriteLine("1:Flip coin\n2:Leap Year\n3:Power of two\n4:Harmonics");
+            Console.WriteLine("1:Flip coin\n2:Leap Year\n3:Power of two\n4:Harmonics\n5:Find Quotient");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -24,6 +24,10 @@
                 case 4:
                     Harmonics harmonics = new Harmonics();
                     harmonics.HarmonicsSeries();
+                    break;
+                case 5:
+                    FindQuotient findQuotient = new FindQuotient();
+                    findQuotient.FindQuotients();
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
