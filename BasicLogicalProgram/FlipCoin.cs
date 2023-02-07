f0bb5ad3bf498aaea9c1b912b557a16a4b930c18
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-07 09:23:31 +0530

    Modified to Flip Coin Program

---
+++ BasicLogicalProgram/FlipCoin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogicalProgram
+{
+    public class FlipCoin
+    {
+        float countHead = 0;
+        float countTail = 0;
+        public void FlipCoins()
+        {
+            Console.WriteLine("Please enter the number of times you want flip coin");
+            int flipNum = Convert.ToInt32(Console.ReadLine());
+
+            Random random = new Random();
+            for (int i = 0; i < flipNum; i++)
+            {
+                double numCheck = random.NextDouble();
+                if (numCheck > 0.5)
+                    countHead++;
+                else
+                    countTail++;
+            }
+            Console.WriteLine("The headcount is" + countHead);
+            Console.WriteLine("The tailcount is" + countTail);
+            float headPercentage = (countHead / flipNum) * 100;
+            float tailPercentage = (countTail / flipNum) * 100;
+            Console.WriteLine("Head count percentage is:" + headPercentage);
+            Console.WriteLine("Tail count percentage is:" + tailPercentage);
+        }
+    }
+}
--- BasicLogicalProgram/Program.cs
+++ BasicLogicalProgram/Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Bassic Logical Program");
+            Console.WriteLine("1:Flip coin");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.FlipCoins();
+                    break;
+                default:
+                    Console.WriteLine("Please enter right option");
+                    break;
+            }    
         }
     }
 }
