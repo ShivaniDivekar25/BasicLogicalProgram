f9cb5b581c2a428ebb606706dc7fa1e0107c55d8
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-07 09:32:54 +0530

    Modified to Power Of Two program

---
+++ BasicLogicalProgram/PowerOfTwo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogicalProgram
+{
+    public class PowerOfTwo
+    {
+        public void PowerOf2()
+        {
+            Console.WriteLine("Enter the number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                double res = Math.Pow(2, i);
+                Console.WriteLine("The power of two is:" + res);
+            }
+        }
+    }
+}
--- BasicLogicalProgram/Program.cs
+++ BasicLogicalProgram/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Bassic Logical Program");
-            Console.WriteLine("1:Flip coin\n2:Leap Year");
+            Console.WriteLine("1:Flip coin\n2:Leap Year\n3:Power of two");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -16,6 +16,10 @@
                 case 2:
                     LeapYear leapYear = new LeapYear();
                     leapYear.LeapYears();
+                    break;
+                case 3:
+                    PowerOfTwo powerOfTwo = new PowerOfTwo();
+                    powerOfTwo.PowerOf2();
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
