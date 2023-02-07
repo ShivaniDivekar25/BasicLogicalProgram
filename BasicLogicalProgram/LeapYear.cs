cf224f4c6baf89fb45997f24ec11fe25148fd0a6
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-07 09:29:18 +0530

    Modified to Leap Year program

---
+++ BasicLogicalProgram/LeapYear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogicalProgram
+{
+    public class LeapYear
+    {
+        public void LeapYears()
+        {
+            Console.WriteLine("Please enter year you want to find");
+            int Year = Convert.ToInt32(Console.ReadLine());
+
+            if ((Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0))
+            {
+                Console.WriteLine("The {0} is leap year", Year);
+            }
+            else
+            {
+                Console.WriteLine("The {0} is not leap year", Year);
+            }
+        }
+    }
+}
--- BasicLogicalProgram/Program.cs
+++ BasicLogicalProgram/Program.cs
@@ -5,13 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Bassic Logical Program");
-            Console.WriteLine("1:Flip coin");
+            Console.WriteLine("1:Flip coin\n2:Leap Year");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     FlipCoin flipCoin = new FlipCoin();
                     flipCoin.FlipCoins();
+                    break;
+                case 2:
+                    LeapYear leapYear = new LeapYear();
+                    leapYear.LeapYears();
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
